cb5d0e2a3a05e02711298eb73e92ad379aef8b7e
cj pfenninger <cjpf@charliejuliet.net>
2020-02-23 23:29:09 -0500

    email methods

---
+++ app/email.py
@@ -0,0 +1,23 @@
+from threading import Thread
+from flask import current_app
+from flask_mail import Message
+from app import mail
+
+
+def send_async_email(app, msg):
+    '''
+    Send email in non-blocking context
+    '''
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_mail(subject, sender, recipients, text_body, html_body):
+    '''
+    Generate an email to send from the configured mail server in a unique thread
+    '''
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    Thread(target=send_async_email,
+           args=(current_app._get_current_object(), msg)).start()
