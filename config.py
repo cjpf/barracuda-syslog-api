7f9ea69dcfe7c578f56cf7340ba847ee8047be64
cj pfenninger <cjpf@charliejuliet.net>
2020-02-24 01:23:44 -0500

    configurations
    organized and restructured

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from app.parse import jobs
+from app.parse import parse
 
 
 BASEDIR = os.path.abspath(os.path.dirname(__file__))
@@ -13,33 +13,22 @@ class BaseConfig(object):
     '''
     SECRET_KEY = os.environ.get('SECRET_KEY') or \
         'some-secret-key'
+    DEBUG = True
+    TESTING = False
+
+    # API Configurations
+    # Enable API by Default.  Set MAIL_API = False in .env to Disable.
+    MAIL_API = os.environ.get('MAIL_API') or \
+        True
+
+    # Database Configurations
+    # Set DATABASE_URL in .env or allow SQLite as default.
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL') or \
         'sqlite:///' + os.path.join(BASEDIR, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['cjpf@charliejuliet.net']
-    ESS_LOG = os.environ.get('ESS_LOG') or \
-        'ess.log'
-    ESS_LOG_OFFSET = os.environ.get('ESS_LOG_OFFSET') or \
-        'ess.log.offset'
-    JOBS = [
-        {
-            'id': 'job1',
-            'func': jobs.parse_log,
-            'trigger': 'cron',
-            'day_of_week': '*',
-            'hour': '*',
-            'minute': '5'
-        }
-    ]
-    # SCHEDULER_JOBSTORES = {
-    #     'default': SQLAlchemyJobStore(url='sqlite:///jobs.db')
-    # }
+
+    # APScheduler Configuration
     SCHEDULER_EXECUTORS = {
         'default': {'type': 'threadpool', 'max_workers': 1}
     }
@@ -47,10 +36,29 @@ class BaseConfig(object):
         'coalesce': False,
         'max_instances': 3
     }
-    SCHEDULER_API_ENABLED = True
+    SCHEDULER_API = True
+
+    # Parser Configurations
     JOB_CONFIG = False
-    DEBUG = True
-    TESTING = False
+    # Parse Job will default to running every 3 minutes in production.
+    JOBS = [
+        {
+            'id': 'job1',
+            'func': parse.parse_log,
+            'trigger': 'cron',
+            'day_of_week': '*',
+            'hour': '*',
+            'minute': '2,5,8,11,14,17,20,23,26,29,32,35,38,41,44,47,50,53,56,59'
+        }
+    ]
+
+    # Mail Configurations
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['cjpf@charliejuliet.net']
 
 
 class DevelopmentConfig(BaseConfig):
@@ -59,7 +67,7 @@ class DevelopmentConfig(BaseConfig):
     '''
     DEBUG = True
     TESTING = True
-    
+
 
 class ProductionConfig(BaseConfig):
     '''
@@ -74,16 +82,23 @@ class TestConfig(BaseConfig):
     '''
     Unit Testing Config
     '''
+    DEBUG = True
     TESTING = True
     SQLALCHEMY_DATABASE_URI = 'sqlite://'
-    SCHEDULER_API_ENABLED = False
+    SCHEDULER_API = False
 
 
 class JobConfig(BaseConfig):
     '''
     Config for running ESS Log parsing jobs
     '''
-    SCHEDULER_API_ENABLED = False
-    JOB_CONFIG = True
-    DEBUG = True
+    DEBUG = False
     TESTING = False
+    SCHEDULER_API = False
+    JOB_CONFIG = True
+    # Default ess.log file will not work for most deployments.
+    # Production environments require this to be set in .env
+    ESS_LOG = os.environ.get('ESS_LOG') or \
+        os.path.join(BASEDIR, 'ess.log')
+    ESS_LOG_OFFSET = os.environ.get('ESS_LOG_OFFSET') or \
+        os.path.join(BASEDIR, 'ess.log.offset')
