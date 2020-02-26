e5dce345fa025371868179208f8a722e3663d6e6
cj pfenninger <cjpf@charliejuliet.net>
2020-02-26 01:17:52 -0500

    mail_server.

--- config.py
+++ config.py
@@ -48,12 +48,12 @@ class BaseConfig(object):
     ]
 
     # Mail Configurations
-    # MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    # MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    # MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    # MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    # ADMINS = ['cjpf@charliejuliet.net']
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['cjpf@charliejuliet.net']
 
 
 class DevelopmentConfig(BaseConfig):
