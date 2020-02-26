5ffe29cac6395ee6b8ef467cfab8d3ed3f3c598a
Xiao kuang <kuangx@oregonstate.edu>
2020-02-25 20:29:52 -0800

    Updated attachments API

--- app/api/attachments.py
+++ app/api/attachments.py
@@ -2,11 +2,11 @@ from app import db
 from app.api import bp
 from app.api.errors import bad_request
 from app.api.auth import token_auth
-from app.models import Attachment
+from app.models import Attachment, Message
 from flask import jsonify, request, url_for
 
 
-@bp.route('/attachments/<string:attachment_id>', methods=[GET])
+@bp.route('/attachments/<int:attachment_id>', methods=['GET'])
 @token_auth.login_required
 def get_attachment(attachment_id):
     '''
@@ -14,23 +14,43 @@ def get_attachment(attachment_id):
     '''
     return jsonify(Attachment.query.get_or_404(attachment_id).to_dict())
 
-@bp.route('/attachments/<string:message_id>', methods=[GET])
+
+@bp.route('/attachments/<string:message_id>', methods=['GET'])
 @token_auth.login_required
-def get_attachments():
+def get_attachments(message_id):
     '''
         Retrieve a collection of all attachments
     '''
-    print("message id : {}".format(message_id))
-    m = Message.query.get_or_404(message_id).first()
-    return jsonify(m.attachments.all())
-    # look at tables and models and check out how to do this
-    # look into this^^
+    page = request.args.get('page', 1, type=int)
+    per_page = min(request.args.get('per_page', 10, type=int), 100)
+    m = Message.query.get_or_404(message_id)
+    data = Attachment.to_collection_dict(
+        m.attachments,  page, per_page, 'api.get_attachments', message_id=message_id)
+    return jsonify(data)
 
-@bp.route('/attachments/<string:message_id>', methods=[POST])
+
+@bp.route('/attachments', methods=['POST'])
 @token_auth.login_required
 def create_attachment():
     '''
         Create a new attachment
     '''
+    data = request.get_json() or {}
+    if 'message_id' not in data:
+        return bad_request('must include message_id')
+    if 'name' not in data:
+        return bad_request('must include name')
+    if Attachment.query.filter_by(
+            message_id=data['message_id'], 
+            name = data['name']).first():
+        return bad_request('message already has that attachment name')
+    attachment = Attachment()
+    attachment.from_dict(data)
+    db.session.add(attachment)
+    db.session.commit()
+    response = jsonify(attachment.to_dict())
+    response.status_code = 201
     
-
+    response.headers['Location'] = url_for(
+        'api.get_attachment', attachment_id=attachment.id)
+    return response
