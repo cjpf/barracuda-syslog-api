9b61f4723b46414374d698241118ae88c41b6138
CJ <cjpf@charliejuliet.net>
2020-02-28 08:22:04 -0500

    Cleaned up Recipients routes
    removed superfluous comments and fixed spacing

--- app/api/recipients.py
+++ app/api/recipients.py
@@ -5,23 +5,21 @@ from app.api.auth import token_auth
 from app.models import Recipient, Message
 from flask import jsonify, request, url_for
 
-# get single recipient by id
+
 @bp.route('/recipients/<int:recipient_id>', methods=['GET'])
 @token_auth.login_required
 def get_recipient(recipient_id):
     '''
-        Retrieve a single recipient
+    Retrieve a single Recipient
     '''
     return jsonify(Recipient.query.get_or_404(recipient_id).to_dict())
-    # return jsonify(Attachment.query.get_or_404(attachment_id).to_dict())
 
 
-# get all recipients by message id
 @bp.route('/recipients/<string:message_id>', methods=['GET'])
 @token_auth.login_required
 def get_recipients(message_id):
     '''
-        Retrieve collection of recpients for a message
+    Retrieve collection of Recipients for a message
     '''
     page = request.args.get('page', 1, type=int)
     per_page = min(request.args.get('per_page', 10, type=int), 100)
@@ -30,12 +28,12 @@ def get_recipients(message_id):
         m.recipients, page, per_page, 'api.get_recipients', message_id=message_id)
     return jsonify(data)
 
-# create a recipient by message id
+
 @bp.route('/recipients', methods=['POST'])
 @token_auth.login_required
 def create_recipient():
     '''
-        Create Recipient
+    Create Recipient
     '''
     data = request.get_json() or {}
     if 'message_id' not in data:
@@ -46,7 +44,7 @@ def create_recipient():
         return bad_request('must include delivery status')
     if Recipient.query.filter_by(
             message_id=data['message_id'],
-            email = data['email']).first():
+            email=data['email']).first():
         return bad_request('message already has this recipient email')
     recipient = Recipient()
     recipient.from_dict(data)
@@ -55,7 +53,7 @@ def create_recipient():
     response = jsonify(recipient.to_dict())
     response.status_code = 201
 
-    # response.headers['Location'] = url_for(
-    #     'api.get_recipient', recipient=recipient.id
-    # )
-    return response    
+    response.headers['Location'] = url_for(
+        'api.get_recipient', recipient=recipient.id
+    )
+    return response
